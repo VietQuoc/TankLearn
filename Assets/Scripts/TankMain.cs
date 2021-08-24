7c3426fe95797c8e9c8b46db3c50a88d819a49d4
Quoc Nguyen <quoc.nguyen@codecomplete.jp>
2021-08-24 10:51:15 +0700

    New Audio

--- Assets/Scripts/TankMain.cs
+++ Assets/Scripts/TankMain.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class TankMain : MonoBehaviour
 {
-    public float speed = 2.1f;
-    public string charactorName = "Main";
-    public float damage = 1;
-    public float hp = 10;
+    public float speed = 2.1f; // movement speed
+    public string charactorName = "Main"; // name
+    public float damage = 1; // damage on one shoot
+    public float hp = 10; // total hp
+    public float currentHp = 10; // current hp
     public Animator anim;
-    public GameObject bullet;
-    public bool isPlayer = true;
+    public GameObject bullet; // input bullet Object
+    public bool isPlayer = true; // is player or bot
+    public float fireRate = 1.5F; // fire rate in second
 
-    private int transformX = 0;
-    private int transformY = 0;
-    private int direction = 0;
-    private bool isDie = false;
+    private float nextFire = 0.0F; // next fire second remain
+    private int transformX = 0; // direction x
+    private int transformY = 0; // direction y
+    private int direction = 0; // direcetion number of 
+    private bool isDie = false; // is dying
+    private int[,] direction2D = new int[,] { { 5, 6, 7 }, { 4, 0, 0 }, { 3, 2, 1 } };
+    private int[] rotationList = { 0, 315, 270, 225, 180, 135, 90, 45 };
+    private int[,] directionList = { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
 
+    // Audio
+    private AudioSource audioSource;
+    public AudioClip[] audioClips; // 0: shoot, 1: attacked, 2: destroy
     private void Awake()
     {
         anim.GetComponent<Animator>();
         bullet.GetComponent<Bullet>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
@@ -82,7 +92,8 @@ public class TankMain : MonoBehaviour
 
         transform.Translate(new Vector2(transformX * speed, transformY * speed) * Time.deltaTime, Space.World);
 
-        if (Input.GetKeyUp(KeyCode.A)) {
+        if (Input.GetKeyUp(KeyCode.A) && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
             OnAttack();
         }
     }
@@ -106,18 +117,32 @@ public class TankMain : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Bullet" && collision.gameObject.GetComponent<Bullet>().parent != gameObject) {
+        OnAttacked(collision);
+    }
+
+    private void OnAttacked(Collider2D collision) {
+        if (collision.gameObject.tag == "Bullet" && collision.gameObject.GetComponent<Bullet>().parent != gameObject)
+        {
             hp = hp - collision.gameObject.GetComponent<Bullet>().damage;
             collision.gameObject.GetComponent<Bullet>().OnAttack();
-            if (hp <= 0) {
+            if (hp <= 0)
+            {
                 anim.SetInteger("tankState", 2);
                 isDie = true;
+                audioSource.clip = audioClips[2];
+                audioSource.Play();
+            }
+            else {
+                audioSource.clip = audioClips[1];
+                audioSource.Play();
             }
         }
     }
 
     private void OnAttack()
     {
+        audioSource.clip = audioClips[0];
+        audioSource.Play();
         GameObject instanceBullet = Instantiate(bullet, new Vector3(0, 0, 0), Quaternion.identity);
         int directionX = directionList[direction, 0];
         int directionY = directionList[direction, 1];
@@ -128,12 +153,10 @@ public class TankMain : MonoBehaviour
         instanceBullet.GetComponent<Bullet>().transformX = directionX;
         instanceBullet.GetComponent<Bullet>().transformY = directionY;
         instanceBullet.GetComponent<Bullet>().parent = gameObject;
+        instanceBullet.GetComponent<Bullet>().speed = speed*2;
     }
     public void destroyObject()
     {
         Destroy(gameObject);
     }
-    private int[,] direction2D = new int[,] { { 5, 6, 7 }, { 4, 0, 0 }, { 3, 2, 1 } };
-    private int[] rotationList = { 0, 315, 270, 225, 180, 135, 90, 45 };
-    private int[,] directionList = { {0, 1}, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
 }
